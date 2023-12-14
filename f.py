f9397d1a7524a0ae7c9326b5898d3abc4d100646
stefa <144994975+StefaniyaP@users.noreply.github.com>
2023-12-14 14:06:51 +0500

    Update f.py

--- f.py
+++ f.py
@@ -1,24 +1,29 @@
 import random
 
-#def invisible_return(func):
-   # def wrapper(x):
-    #    x, _ = func(x)
-    #    return x
-   # return wrapper
+# декоратор invisible_return принимает выводимые параметр и строку функции low_hp (либо high_hp) 
+# и подавляет вывод строки, отправляя на вывод только параметр x
+def invisible_return(func):
+    def wrapper(x):
+        x, _ = func(x)
+        return x
+    return wrapper
     
 def hp():
-    x = random.randint(0, 100)
+    # значение хп рандомно задается в диапазоне от 0 до 100
+    x = random.randint(0, 101)
     print(f'у вас {x} хп')
 
-    #@invisible_return
+    # функция high_hp понижает число хп
+    @invisible_return
     def high_hp(x):
         r = random.randint(0, 50)   
         x -= r       
         if x <= 1:
             return 1, f'Хп понижено на {r}'
         return x, f'Хп понижено на {r}'
-    
-    #@invisible_return
+
+    # функция low_hp повышает число хп
+    @invisible_return
     def low_hp(x):
         r = random.randint(0, 50)
         x += r
@@ -26,6 +31,8 @@ def hp():
             return 100, f'Хп повышено на {r}'
         return x, f'Хп повышено на {r}'
 
+    # если x лежит в диапазоне от 50 до 100 включительно, то пользователю дается 
+    # выбор понизить хп или же оставить его прежним
     if 50 <= x <= 100:
         choice = int(input('Желаете понизить хп?\n'
                             '1. да\n'
@@ -34,7 +41,8 @@ def hp():
             f = high_hp(x)
             return f
         else: 
-            return f'по прежнему {x} хп'
+            return f'по-прежнему {x} хп'
+    # если x лежит в диапазоне до 50, то пользователю дается выбор повысить хп или же оставить его прежним
     elif x < 50:
         choice = int(input('Желаете повысить хп?\n'
                             '1. да\n'
@@ -43,7 +51,7 @@ def hp():
             f = low_hp(x)
             return f
         else:
-            return f'по прежнему {x} хп'
+            return f'по-прежнему {x} хп'
 
 print(hp())
 
