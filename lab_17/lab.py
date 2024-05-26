8762dd59f803f2d8b88781a3368e0f68bc23aaba
StefaniyaP <polozhenko_se@edu.surgu.ru>
2024-05-26 17:56:02 +0500

    add

--- lab_17/lab.py
+++ lab_17/lab.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -9,26 +8,24 @@ Base = declarative_base()
 
 class Teacher(Base):
     __tablename__ = 'teachers'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    teacher = relationship('Discipline', back_populates='teachers_name')
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 
 class Discipline(Base):
     __tablename__ = 'disciplines'
-    id = Column(Integer, primary_key=True)
-    teacher_id = Column(Integer, ForeignKey('teachers.id'))
-    name_of_the_discipline = Column(String)
-    discipline = relationship('EducationalPlan', back_populates='name_of_the_d')
-    teachers_name = relationship('Teacher', back_populates='teacher')
+    id = Column(Integer(), primary_key=True)
+    name_of_the_discipline = Column(String())
 
 
 class EducationalPlan(Base):
     __tablename__ = 'educational_plans'
-    id = Column(Integer, primary_key=True)
-    discipline_id = Column(Integer, ForeignKey('disciplines.id'))
-    name_of_the_plan = Column(String)
-    name_of_the_d = relationship('Discipline', back_populates='discipline')
+    id = Column(Integer(), primary_key=True)
+    name_of_the_plan = Column(String())
+    discipline_id = Column(Integer(), ForeignKey('disciplines.id'))
+    teacher_id = Column(Integer(), ForeignKey('teachers.id'))
+    teacher = relationship('Teacher')
+    discipline = relationship('Discipline')
 
 
 # создание бд
@@ -45,38 +42,32 @@ discipline3 = Discipline(name_of_the_discipline='История')
 
 teacher1 = Teacher(name="Иванов И.И.")
 teacher2 = Teacher(name="Петров П.П.")
-teacher3 = Teacher(name="Петров П.П.")
 
-educational_plan1 = EducationalPlan(name_of_the_plan="План 1")
-educational_plan2 = EducationalPlan(name_of_the_plan="План 2")
-educational_plan3 = EducationalPlan(name_of_the_plan='План 3')
+ep1 = EducationalPlan(name_of_the_plan="План 1", discipline=discipline1, teacher=teacher1)
+ep2 = EducationalPlan(name_of_the_plan="План 2", discipline=discipline2, teacher=teacher1)
+ep3 = EducationalPlan(name_of_the_plan='План 3', discipline=discipline3, teacher=teacher2)
 
 # Запросы
 
 # вывод всех учителей и дисциплин
-teachers_with_disciplines = session.query(Teacher, Discipline).filter(Teacher.id == Discipline.teacher_id).all()
-for teacher, discipline in teachers_with_disciplines:
-    print(f"Учитель: {teacher.name}, Дисциплина: {discipline.name_of_the_discipline}")
+# teachers_with_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).all()
+# for i in range(0, 3):
+#     print(f'ID учебного плана: {teachers_with_disciplines[i][0]}, Преподаватель: {teachers_with_disciplines[i][1]}, Дисциплина: {teachers_with_disciplines[i][2]}')
 
 # вывод всех учебных планов и дисциплин, связанных с ними
-educational_plans_with_disciplines = session.query(EducationalPlan, Discipline).filter(EducationalPlan.discipline_id == Discipline.id).all()
-for educational_plan, discipline in educational_plans_with_disciplines:
-    print(f"Учебный план: {educational_plan.name_of_the_plan}, Дисциплина: {discipline.name_of_the_discipline}")
+educational_plans_with_disciplines = session.query(EducationalPlan.name_of_the_plan, Discipline.name_of_the_discipline).join(Discipline).filter(EducationalPlan.discipline_id == Discipline.id).all()
+# print(educational_plans_with_disciplines)
+for i in range(0, 3):
+    print(f'Наименование учебного плана: {educational_plans_with_disciplines[i][0]}, Дисциплина: {educational_plans_with_disciplines[i][1]}')
 
-# вывод всех учебных планов и связанных с ними учителей через дисциплины
-educational_plans_with_teachers = session.query(EducationalPlan, Discipline, Teacher).filter(EducationalPlan.discipline_id == Discipline.id).filter(Discipline.teacher_id == Teacher.id).all()
-for educational_plan, discipline, teacher in educational_plans_with_teachers:
-    print(f"Учебный план: {educational_plan.name_of_the_plan}, Дисциплина: {discipline.name_of_the_discipline}, Учитель: {teacher.name}")
+# вывод тех дисциплин, которые ведет Иванов И.И.
+# teachers1_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).filter(Teacher.name == 'Иванов И.И.').all()
+# for i in range(0, 2):
+#     print(f'ID учебного плана: {teachers1_disciplines[i][0]}, ФИО преподавателя: {teachers1_disciplines[i][1]}, Дисциплина: {teachers1_disciplines[i][2]}')
 
-session.add(discipline1)
-session.add(discipline2)
-session.add(discipline3)
-session.add(teacher1)
-session.add(teacher2)
-session.add(teacher3)
-session.add(educational_plan1)
-session.add(educational_plan2)
-session.add(educational_plan3)
+session.add_all([discipline1, discipline2, discipline3])
+session.add_all([teacher1, teacher2])
+session.add_all([ep1, ep2, ep3])
 session.commit()
 session.close()
 
--- lab_17/readme.md
+++ lab_17/readme.md
@@ -5,8 +5,131 @@
 3. Написать запросы для выборки и анализа данных из БД.
 4. Оформить отчет в `readme.md`.
 
-## Мой вариант:
+## Мой вариант
 ![image](https://github.com/StefaniyaP/programming/assets/144994975/c2fc1296-df2e-4d48-aef9-72fbd16f6552)
 
-## Ход работы:
+## Ход работы
 
+Спроектированная при помощи crow’s foot notation БД:    
+
+![img.png](img.png)      
+
+Модели данных, заполнение БД, запросы:    
+
+```Python
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+
+class Discipline(Base):
+    __tablename__ = 'disciplines'
+    id = Column(Integer(), primary_key=True)
+    name_of_the_discipline = Column(String())
+
+
+class EducationalPlan(Base):
+    __tablename__ = 'educational_plans'
+    id = Column(Integer(), primary_key=True)
+    name_of_the_plan = Column(String())
+    discipline_id = Column(Integer(), ForeignKey('disciplines.id'))
+    teacher_id = Column(Integer(), ForeignKey('teachers.id'))
+    teacher = relationship('Teacher')
+    discipline = relationship('Discipline')
+
+
+# создание бд
+engine = create_engine('sqlite:///university.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Заполнение данными
+discipline1 = Discipline(name_of_the_discipline="Алгебра")
+discipline2 = Discipline(name_of_the_discipline="Физика")
+discipline3 = Discipline(name_of_the_discipline='История')
+
+teacher1 = Teacher(name="Иванов И.И.")
+teacher2 = Teacher(name="Петров П.П.")
+
+ep1 = EducationalPlan(name_of_the_plan="План 1", discipline=discipline1, teacher=teacher1)
+ep2 = EducationalPlan(name_of_the_plan="План 2", discipline=discipline2, teacher=teacher1)
+ep3 = EducationalPlan(name_of_the_plan='План 3', discipline=discipline3, teacher=teacher2)
+
+# Запросы
+
+# вывод всех учителей и дисциплин
+# teachers_with_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).all()
+# for i in range(0, 3):
+#     print(f'ID учебного плана: {teachers_with_disciplines[i][0]}, Преподаватель: {teachers_with_disciplines[i][1]}, Дисциплина: {teachers_with_disciplines[i][2]}')
+
+# вывод всех учебных планов и дисциплин, связанных с ними
+# educational_plans_with_disciplines = session.query(EducationalPlan.name_of_the_plan, Discipline.name_of_the_discipline).join(Discipline).filter(EducationalPlan.discipline_id == Discipline.id).all()
+# for i in range(0, 3):
+#     print(f'Наименование учебного плана: {educational_plans_with_disciplines[i][0]}, Дисциплина: {educational_plans_with_disciplines[i][1]}')
+
+# вывод тех дисциплин, которые ведет Иванов И.И.
+teachers1_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).filter(Teacher.name == 'Иванов И.И.').all()
+for i in range(0, 2):
+    print(f'ID учебного плана: {teachers1_disciplines[i][0]}, ФИО преподавателя: {teachers1_disciplines[i][1]}, Дисциплина: {teachers1_disciplines[i][2]}')
+
+session.add_all([discipline1, discipline2, discipline3])
+session.add_all([teacher1, teacher2])
+session.add_all([ep1, ep2, ep3])
+session.commit()
+session.close()
+```
+
+Выполнение запросов:     
+
+Запрос 1    
+
+```Python
+# вывод всех учителей и дисциплин
+teachers_with_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).all()
+for i in range(0, 3):
+    print(f'ID учебного плана: {teachers_with_disciplines[i][0]}, Преподаватель: {teachers_with_disciplines[i][1]}, Дисциплина: {teachers_with_disciplines[i][2]}')
+```
+
+Вывод:    
+
+![1716724438595](image/readme/1716724438595.png)
+
+Запрос 2   
+
+```Python
+# вывод всех учебных планов и дисциплин, связанных с ними
+educational_plans_with_disciplines = session.query(EducationalPlan.name_of_the_plan, Discipline.name_of_the_discipline).join(Discipline).filter(EducationalPlan.discipline_id == Discipline.id).all()
+for i in range(0, 3):
+    print(f'Наименование учебного плана: {educational_plans_with_disciplines[i][0]}, Дисциплина: {educational_plans_with_disciplines[i][1]}')
+```
+
+Вывод:   
+
+![1716727038011](image/readme/1716727038011.png)
+
+Запрос 3  
+
+```Python
+# вывод тех дисциплин, которые ведет Иванов И.И.
+teachers1_disciplines = session.query(EducationalPlan.id, Teacher.name, Discipline.name_of_the_discipline).join(Discipline).join(Teacher).filter(Teacher.name == 'Иванов И.И.').all()
+for i in range(0, 2):
+    print(f'ID учебного плана: {teachers1_disciplines[i][0]}, ФИО преподавателя: {teachers1_disciplines[i][1]}, Дисциплина: {teachers1_disciplines[i][2]}')
+```
+
+Вывод:   
+
+![1716727498922](image/readme/1716727498922.png)
+
+## Используемые материалы
+[Создание схемы в SQLAlchemy ORM](https://pythonru.com/biblioteki/shemy-v-sqlalchemy-orm)
